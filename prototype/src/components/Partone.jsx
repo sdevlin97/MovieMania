7e9842638e57067a2d183d1e82db8e931eee9531
Kevin Horta <kevinhorta3@gmail.com>
2023-07-27 22:23:10 -0400

    Mutiple options in sign in/ sign up added

--- prototype/src/components/Partone.jsx
+++ prototype/src/components/Partone.jsx
@@ -23,20 +23,14 @@ function Partone() {
         >
           Log In<i className="fa-solid fa-right-to-bracket ml-2"></i>
         </button>
-        <Pop trigger={ButtonPopup} setTrigger={setButtonPopup}>
-          <h1>popup</h1>
-          <p>Future Log on form</p>
-        </Pop>
+        <Pop trigger={ButtonPopup} setTrigger={setButtonPopup}></Pop>
         <button
           onClick={() => setButtonPopup(true)}
           className="ml-4 z-50 bg-transparent hover:bg-cyan-600 text-cyan-500 font-semibold hover:text-white py-2 px-4 border border-cyan-500 border-4 hover:border-transparent rounded-full text-[24px] hover:transition ease-in-out duration-300 hover:-translate-y-1"
         >
           Sign Up<i className="fa-solid fa-user-plus ml-2"></i>
         </button>
-        <Pop trigger={ButtonPopup} setTrigger={setButtonPopup}>
-          <h1>popups</h1>
-          <p>Future Log on form</p>
-        </Pop>
+        <Pop trigger={ButtonPopup} setTrigger={setButtonPopup}></Pop>
       </div>
     </Parallax>
   );
--- prototype/src/components/Pop.jsx
+++ prototype/src/components/Pop.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 function Pop(props) {
+  const [activeTab, setActiveTab] = useState("login");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return props.trigger ? (
     <div className="z-50 fixed top-0 left-0 w-full h-screen justify-center items-center flex">
       <div className="bg-white relative p-8 w-full max-w-max">
@@ -10,7 +17,39 @@ function Pop(props) {
         >
           X
         </button>
-        {props.children}
+        <div className="">
+          <button
+            onClick={() => handleTabClick("login")}
+            className={`py-2 px-4 border-2   ${
+              activeTab === "login" ? "border-red-500" : ""
+            }`}
+          >
+            Login
+          </button>
+          <button
+            onClick={() => handleTabClick("register")}
+            className={`py-2 px-4 border-2  ${
+              activeTab === "register" ? "border-red-500" : ""
+            }`}
+          >
+            Register
+          </button>
+        </div>
+        <div className="tab-content">
+          {activeTab === "login" && (
+            <div className="text-center mb-3">
+              <p>Sign in with:</p>
+              {/* Login Form Inputs */}
+            </div>
+          )}
+
+          {activeTab === "register" && (
+            <div className="text-center mb-3">
+              <p>Sign up with:</p>
+              {/* Register Form Inputs */}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   ) : (
