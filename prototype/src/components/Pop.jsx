712e485f7999d9eb8f0ad64861feb1fb8672859d
Kevin Horta <kevinhorta3@gmail.com>
2023-07-28 00:28:02 -0400

    Update Pop.jsx

--- prototype/src/components/Pop.jsx
+++ prototype/src/components/Pop.jsx
@@ -17,17 +17,17 @@ function Pop(props) {
 
   return props.trigger ? (
     <div className="z-50 fixed top-0 left-0 w-full h-screen justify-center items-center flex">
-      <div className="bg-white relative p-8 w-full max-w-max">
+      <div className="backdrop-blur relative  p-8 w-full max-w-max border border-cyan-700">
         <button
           onClick={() => props.setTrigger(false)}
-          className="text-red-500 absolute top-0 right-0 px-2"
+          className=" absolute top-0 right-0 px-2"
         >
           X
         </button>
         <div className="">
           <button
             onClick={() => handleTabClick("login")}
-            className={`py-2 px-4 border-2   ${
+            className={`py-2 px-4 border-2 text-cyan-500  ${
               activeTab === "login" ? "border-red-500" : ""
             }`}
           >
@@ -35,7 +35,7 @@ function Pop(props) {
           </button>
           <button
             onClick={() => handleTabClick("register")}
-            className={`py-2 px-4 border-2  ${
+            className={`py-2 px-4 border-2 text-cyan-500 ${
               activeTab === "register" ? "border-red-500" : ""
             }`}
           >
@@ -44,7 +44,7 @@ function Pop(props) {
         </div>
         <div className="tab-content">
           {activeTab === "login" && (
-            <div className="text-center mb-3">
+            <div className="text-center mb-3 text-cyan-500">
               <p>Sign in with:</p>
               <h></h>
               <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
@@ -54,6 +54,64 @@ function Pop(props) {
           {activeTab === "register" && (
             <div className="text-center mb-3">
               <p>Sign up with:</p>
+              <div className="mb-4">
+                <label for="name" className="block text-cyan-500">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  className="border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label for="username" className="block text-cyan-500">
+                  Username
+                </label>
+                <input
+                  id="username"
+                  type="text"
+                  className="border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label for="email" className="block text-cyan-500">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  className="border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label for="password" className="block text-cyan-500">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  className="border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label for="terms" className="flex items-center text-cyan-500">
+                  <input
+                    type="checkbox"
+                    name="flexCheck"
+                    id="terms"
+                    className="form-checkbox border  border-gray-400 rounded-md text-blue-500 focus:outline-none focus:border-blue-500"
+                  />
+                  <span className="ml-2">
+                    I have read and agree to the terms
+                  </span>
+                </label>
+              </div>
+
               <GoogleLogin
                 onSuccess={responseMessage}
                 onError={errorMessage}
