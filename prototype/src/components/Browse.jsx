a0b0b5c1004bf36e395585f1e99681398b8eb652
Kevin Horta <100811318+vzkevin@users.noreply.github.com>
2023-08-25 15:51:19 -0400

    making browse section

--- prototype/src/components/Browse.jsx
+++ prototype/src/components/Browse.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { Parallax } from "react-parallax";
+import Card from "./Card";
 
 function Browse() {
   return (
-    <div>
-      <div className=" text-white bg-black">hello</div>
-    </div>
+    <>
+      <Parallax
+        bgImage="./pawel-czerwinski-XM1YUUVXj64-unsplash.jpg"
+        strength={800}
+        className="relative h-screen bg-black"
+      >
+        <div className=" h-m-[5] grid grid-flow-col gap-4">
+          ssss
+          <div>
+            <Card></Card>
+          </div>
+          sss
+          <div>
+            <Card></Card>
+          </div>
+          <div>
+            <Card></Card>
+          </div>
+        </div>
+      </Parallax>
+    </>
   );
 }
 
---
+++ prototype/src/components/Card.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Card() {
+  return <div className="h-[50px] w-[50px] bg-green-500 flex"></div>;
+}
