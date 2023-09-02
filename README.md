f85823a966fbb8ec12e2839fe20cb04c6263a836
Siobahn Devlin <sdevlin2021@fau.edu>
2023-09-01 23:28:49 -0400

    fixed the firebase backend

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MovieMania
 Group 12's Project for Engineering Design 1 and 2
+
+Deployment instructions
+1. cd into MovieMania/prototype
+2. run vite build after you change whatever it is you need to change
+3. cd into MovieMania/prototype/dist and run firebase deploy
--- prototype/src/components/Partone.jsx
+++ prototype/src/components/Partone.jsx
@@ -2,6 +2,25 @@ import { Parallax } from "react-parallax";
 import { Link } from "react-router-dom";
 import Logins from "./Logins";
 import { useState } from "react";
+import $ from 'jquery';
+
+function testBackendCall() {
+  $.ajax({
+    url: `https://us-central1-moviemania-ba604.cloudfunctions.net/app/test`,
+    crossOrigin: true,
+    type: "POST",
+    async: true,
+    success: function (response) {
+        console.log("We've made a sucessful post request!");
+        console.log("The response is: ", response);
+    },
+    error: function (error) {
+        console.log("Something went wrong with our test");
+        console.log("The error is: ");
+        console.log(error);
+    }
+})
+}
 
 function Partone() {
   const [ButtonPopup, setButtonPopup] = useState(false);
@@ -25,6 +44,10 @@ function Partone() {
           Log In / Sign up
           <i className="fa-solid fa-right-to-bracket ml-2"></i>
         </button>
+
+<button onClick={() => testBackendCall()}
+className="z-20  backdrop-blur-lg bg-transparent hover:bg-cyan-600 text-cyan-500 font-semibold hover:text-white py-2 px-4 border border-cyan-500 border-4 hover:border-transparent rounded-full text-[24px] hover:transition ease-in-out duration-300 hover:-translate-y-1">This is Siobahn's test button for the stupid god damn network call<i className="fa-solid fa-right-to-bracket ml-2"></i></button>
+        
         <Logins trigger={ButtonPopup} setTrigger={setButtonPopup}></Logins>
       </div>
     </Parallax>
