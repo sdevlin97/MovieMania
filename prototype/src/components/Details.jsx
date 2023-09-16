8b770f41d4414c99e641d4bc8c43000039e84323
Kevin Horta <100811318+vzkevin@users.noreply.github.com>
2023-09-16 13:15:35 -0400

    Halfway complete thorugh Details page of a movie

--- prototype/src/components/Details.jsx
+++ prototype/src/components/Details.jsx
@@ -1,42 +1,118 @@
-import React from "react";
+// File: Details.js
+
+import React, { useState } from "react";
 import { Parallax } from "react-parallax";
 
 const Details = () => {
+  const [showTrailer, setShowTrailer] = useState(false);
+
   const tags = [
     {
       title: "The Dark Knight",
       rating: "8.5",
       poster: "./batmanfull.jpg",
       description:
-        "The plot follows the vig police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far the Batman will go to save the city from chaos. The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far the Batman will go to save the city from chaos.  ",
-      tags: ["Action", "Crime", "Drama"],
+        "The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far the Batman will go to save the city from chaos. The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far the Batman will go to save the city from chaos. The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far the Batman will go to save the city from chaos. The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far the Batman will go to save the city from chaos. The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far the Batman will go to save the city from chaos.",
+      tags: [
+        "Mind-Bending",
+        "Epic Romance",
+        "Sci-Fi Action",
+        "Quentin Tarantino",
+        "Time Travel",
+        "Thriller",
+        "Alternate Reality",
+        "Existential",
+        "Dystopian",
+        "Cyberpunk",
+        "Art House",
+        "Surreal",
+        "Mind-Bending",
+        "Epic Romance",
+        "Sci-Fi Action",
+        "Quentin Tarantino",
+        "Time Travel",
+        "Thriller",
+        "Alternate Reality",
+        "Existential",
+        "Dystopian",
+        "Cyberpunk",
+        "Art House",
+        "Surreal",
+        "Mind-Bending",
+        "Epic Romance",
+        "Sci-Fi Action",
+        "Quentin Tarantino",
+        "Time Travel",
+        "Thriller",
+        "Alternate Reality",
+        "Existential",
+        "Dystopian",
+        "Cyberpunk",
+        "Art House",
+        "Surreal",
+        "Mind-Bending",
+        "Epic Romance",
+        "Sci-Fi Action",
+        "Quentin Tarantino",
+        "Time Travel",
+        "Thriller",
+        "Alternate Reality",
+        "Existential",
+        "Dystopian",
+        "Cyberpunk",
+        "Art House",
+        "Surreal",
+      ],
     },
     // Add more movie data here...
   ];
 
   return (
-    <Parallax
-      bgImage="./pawel-czerwinski-XM1YUUVXj64-unsplash.jpg"
-      strength={800}
-      className="h-screen w-screen flex items-center justify-center"
-    >
+    <div className="flex flex-wrap justify-center">
       {tags.map((movie, index) => (
         <div
           key={index}
-          className="bg-black p-4 rounded-md mx-2 overflow-y-auto"
+          className="p-4 backdrop-blur-md rounded-lg mx-2 my-4"
+          style={{ width: "300px", flex: "1 1 300px" }}
         >
-          <img
-            src={movie.poster}
-            alt={movie.title}
-            className="mx-auto w-[300px] h-[400px] object-fill rounded-md mb-2"
-          />
+          {showTrailer ? (
+            <div>
+              <video
+                src="batmantrailer.mp4"
+                controls
+                className="mx-auto w-full rounded-md mb-2"
+                style={{ maxHeight: "400px", maxWidth: "100%" }}
+              />
+              <button
+                onClick={() => setShowTrailer(false)}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4 mx-auto block"
+              >
+                Go Back to Image
+              </button>
+            </div>
+          ) : (
+            <div>
+              <img
+                src={movie.poster}
+                alt={movie.title}
+                className="mx-auto w-full rounded-md mb-2"
+                style={{ maxHeight: "400px", width: "auto", maxWidth: "100%" }}
+              />
+              <button
+                onClick={() => setShowTrailer(true)}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4 mx-auto block"
+              >
+                Watch Trailer
+              </button>
+            </div>
+          )}
           <h2 className="text-lg font-semibold text-white text-center">
             {movie.title}
           </h2>
           <p className="text-gray-400 text-sm text-center">
             Rating: {movie.rating}
           </p>
-          <div className="text-white text-center ">
+          <div className="text-white text-center mb-2">
             Description: {movie.description}
           </div>
           <p className="text-white text-center">
@@ -44,7 +120,8 @@ const Details = () => {
           </p>
         </div>
       ))}
-    </Parallax>
+    </div>
   );
 };
+
 export default Details;
