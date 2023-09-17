e0158684e6ed50a5f5b108bdfc8d80e234af4ef8
Siobahn Devlin <sdevlin2021@fau.edu>
2023-09-17 15:40:06 -0400

    Authentication functionality added

--- prototype/src/firebase.js
+++ prototype/src/firebase.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAJDp8nZCV3A-T5IeC7_qG91k9jPEGrdFU",
   authDomain: "moviemania-ba604.firebaseapp.com",
@@ -17,7 +13,52 @@ const firebaseConfig = {
   measurementId: "G-5M4YJ7877N"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export function createAccount(auth, name, email, password) {
+  createUserWithEmailAndPassword(auth, email, password)
+  .then(async (userCredential) => {
+    // account creation success; user is now logged in
+    const user = userCredential.user;
+    console.log("User account creation successful! The user is: ", user.email);
+    // add user to Firestore database
+    addNewUserToDatabase(db, user, name)
+  })
+  .catch((error) => {
+    // error signing in 
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log("Error creating account: ", errorCode, errorMessage);
+  });
+}
+
+export function logIntoExistingAccount(auth, email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("The user signed in successfully! The user is: ", user.email);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log("Error signing into account. The error is: ", errorCode, errorMessage);
+  });
+}
+
+// firestore helper functions
+async function addNewUserToDatabase(db, user, name) {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      name: name,
+      email: user.email
+    });
+
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
