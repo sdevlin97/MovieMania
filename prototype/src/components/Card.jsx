7295e8f98a8e44c10f6e83c701276d5e5974a7ca
Kevin Horta <kevinhorta3@gmail.com>
2023-10-20 18:03:15 -0400

    Centering movie title

--- prototype/src/components/Card.jsx
+++ prototype/src/components/Card.jsx
@@ -45,7 +45,7 @@ const Card = () => {
   const scrollRight = () => {
     if (scrollContainerRef.current) {
       scrollContainerRef.current.scrollTo({
-        left: scrollContainerRef.current.scrollLeft + 500, // Adjust the scroll distance as needed
+        left: scrollContainerRef.current.scrollLeft + 500,
         behavior: "smooth",
       });
     }
@@ -86,7 +86,7 @@ const Card = () => {
           >
             <div className="flex space-x-4 p-4 pr-16">
               {data.results.map((movie, index) => (
-                <div className="p-4 rounded-lg shadow-md backdrop-blur-0 inline-block">
+                <div className="flex flex-col items-center p-4 rounded-lg shadow-md backdrop-blur-0 inline-block">
                   <Link to={`/details/${movie.id}`}>
                     <img
                       src={
@@ -102,7 +102,6 @@ const Card = () => {
                   <p className="text-gray-400 text-sm text-center">
                     Rating: {movie.vote_average}
                   </p>
-                  <div className="mt-2 mx-4 flex justify-center"></div>
                 </div>
               ))}
             </div>
