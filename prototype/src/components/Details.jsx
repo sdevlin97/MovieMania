906010cbbe48de32f6736922d98c3211d2fa61a9
Kevin Horta <kevinhorta3@gmail.com>
2023-10-20 12:16:19 -0400

    Update Details.jsx

--- prototype/src/components/Details.jsx
+++ prototype/src/components/Details.jsx
@@ -21,48 +21,54 @@ const Details = () => {
   ];
 
   return (
-    <div className="flex justify-center items-center h-full py-4">
+    <>
       {tags.map((movie, index) => (
         <div
-          key={index}
-          className="flex rounded-lg mx-2 my-4 w-full max-w-6xl p-4"
+          className="flex justify-center items-center h-full py-4"
           style={{
-            backgroundImage: `url(${movie.backdrop})`,
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${movie.backdrop})`,
+
             backgroundSize: "cover",
             backgroundPosition: "center center",
           }}
         >
-          {showTrailer ? (
-            <div className="w-1/2"></div>
-          ) : (
-            <div className="w-[30%] ">
-              <img
-                src={movie.poster}
-                alt={movie.title}
-                className="w-[100%] h-[100%] rounded-l-md"
-              />
-            </div>
-          )}
+          <div
+            key={index}
+            className="flex rounded-lg mx-2 my-4 w-full max-w-6xl p-4 "
+          >
+            {showTrailer ? (
+              <div className="w-1/2"></div>
+            ) : (
+              <div className="w-[30%] ">
+                <img
+                  src={movie.poster}
+                  alt={movie.title}
+                  className="w-[100%] h-[100%] rounded-l-md"
+                />
+              </div>
+            )}
 
-          <div className="w-[100%] p-4">
-            <h2 className="text-xl font-semibold text-white mb-2">
-              {movie.title}
-            </h2>
-            <p className="text-gray-400 text-base mb-4">
-              {movie.dateReleased} • {movie.genres.join(", ")} • {movie.runtime}
-            </p>
-            <p className="text-white mb-4 italic text-lg">
-              "{movie.quotation}"
-            </p>
-            <div className="text-white text-lg mb-4">{movie.description}</div>
-            <p className="text-gray-400 text-base mb-2">
-              Director: {movie.director}
-            </p>
-            <p className="text-gray-400 text-base">Writer: {movie.writer}</p>
+            <div className="w-[100%] p-4">
+              <h2 className="text-xl font-semibold text-white mb-2">
+                {movie.title}
+              </h2>
+              <p className="text-gray-400 text-base mb-4">
+                {movie.dateReleased} • {movie.genres.join(", ")} •{" "}
+                {movie.runtime}
+              </p>
+              <p className="text-gray-100 mb-4 italic text-lg">
+                "{movie.quotation}"
+              </p>
+              <div className="text-white text-lg mb-4">{movie.description}</div>
+              <p className="text-white text-base mb-2">
+                Director: {movie.director}
+              </p>
+              <p className="text-white text-base">Writer: {movie.writer}</p>
+            </div>
           </div>
         </div>
       ))}
-    </div>
+    </>
   );
 };
 export default Details;
