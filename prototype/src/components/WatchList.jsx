3b5257b07965cdcde60c9451ec54181151a9b2c7
Kevin Horta <kevinhorta3@gmail.com>
2023-11-06 17:21:50 -0500

    Update WatchList.jsx

--- prototype/src/components/WatchList.jsx
+++ prototype/src/components/WatchList.jsx
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+const MovieTitles = [
+  {
+    title: "Inception",
+    tmdbId: 27205,
+  },
+  {
+    title: "The Matrix",
+    tmdbId: 603,
+  },
+  {
+    title: "Interstellar",
+    tmdbId: 157336,
+  },
+  {
+    title: "The Dark Knight",
+    tmdbId: 155,
+  },
+  {
+    title: "Pulp Fiction",
+    tmdbId: 680,
+  },
+];
 
 const WatchList = () => {
+  // Using useState to manage the list of movies e.i remove movies
+  const [movies, setMovies] = useState(MovieTitles);
+
   const containerStyle = {
     backgroundImage: `url('./pawel-czerwinski-XM1YUUVXj64-unsplash.jpg')`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundAttachment: "fixed",
+    color: "white",
+    padding: "20px",
+  };
+
+  // Function to remove a movie from the list it works for now - kevin
+  const removeMovie = (tmdbId) => {
+    setMovies(movies.filter((movie) => movie.tmdbId !== tmdbId));
   };
 
   return (
-    <div style={containerStyle} className="relative h-screen bg-black">
-      <div className="flex items-center justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-white font-serif font-bold text-center tracking-[.20em] sm:text-[50px] md:text-[100px] lg:text-[100px]">
-          Future Watchlist...
-        </h1>
-      </div>
-      {/* The next div seems to be empty and might not be needed */}
-      <div className="my-20 flex items-center justify-center absolute h-screen w-full "></div>
+    <div style={containerStyle} className="relative h-screen">
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.tmdbId} className="mb-4">
+            {movie.title}
+            <button
+              onClick={() => removeMovie(movie.tmdbId)}
+              className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
