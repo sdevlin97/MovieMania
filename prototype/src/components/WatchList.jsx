549c27f237b296265d4a5758521522854dfc4a01
Kevin Horta <kevinhorta3@gmail.com>
2023-11-06 19:50:15 -0500

    Update WatchList.jsx

--- prototype/src/components/WatchList.jsx
+++ prototype/src/components/WatchList.jsx
@@ -1,62 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const MovieTitles = [
   {
-    title: "Inception",
-    id: 27205,
-  },
-  {
     title: "The Matrix",
     id: 603,
+    poster_path: "/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
+  },
+  {
+    title: "Joker",
+    id: 475557,
+    poster_path: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
   },
   {
     title: "Interstellar",
     id: 157336,
-  },
-  {
-    title: "Five Nights At Freddys",
-    id: 507089,
-  },
-  {
-    title: "Pulp Fiction",
-    id: 680,
+    poster_path: "/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
   },
 ];
 
 const WatchList = () => {
-  // Using useState to manage the list of movies e.i remove movies
   const [movies, setMovies] = useState(MovieTitles);
+  const scrollContainerRef = useRef(null);
 
-  const containerStyle = {
-    backgroundImage: `url('./pawel-czerwinski-XM1YUUVXj64-unsplash.jpg')`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundAttachment: "fixed",
-    color: "white",
-    padding: "20px",
-  };
-
-  // Function to remove a movie from the list it works for now - kevin
   const removeMovie = (id) => {
     setMovies(movies.filter((movie) => movie.id !== id));
   };
 
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollTo({
+        left: scrollContainerRef.current.scrollLeft + 500,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollTo({
+        left: scrollContainerRef.current.scrollLeft - 500,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div style={containerStyle} className="relative h-screen">
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id} className="mb-4 ">
-            <Link to={`/details/${movie.id}`}> {movie.title}</Link>
-            <button
-              onClick={() => removeMovie(movie.id)}
-              className="ml-4 bg-red-200 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-            >
-              Remove
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div
+      className="relative h-screen p-20"
+      style={{
+        backgroundImage: `url('./pawel-czerwinski-XM1YUUVXj64-unsplash.jpg')`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        color: "white",
+      }}
+    >
+      <h1 className="tracking-[.50em] font-bold text-center text-[30px] text-white pb-10">
+        Watchlist
+      </h1>
+      <button
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 backdrop-blur-none text-white px-[12px] py-[50px] rounded-md hover:bg-black z-10 text-lg font-black"
+        onClick={scrollLeft}
+      >
+        &lt;
+      </button>
+      <button
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 backdrop-blur-none text-white px-[12px] py-[50px] rounded-md hover:bg-black z-10 text-lg font-black"
+        onClick={scrollRight}
+      >
+        &gt;
+      </button>
+      <div
+        className="overflow-x-auto whitespace-nowrap"
+        ref={scrollContainerRef}
+      >
+        <div className="flex space-x-4 pr-16">
+          {movies.map((movie) => (
+            <div key={movie.id} className="inline-block align-top">
+              <Link to={`/details/${movie.id}`}>
+                <img
+                  src={
+                    "https://image.tmdb.org/t/p/original/" + movie.poster_path
+                  }
+                  alt={movie.title}
+                  className="max-w-xs max-h-xs object-fill rounded-md mb-2"
+                />
+              </Link>
+              <h2 className="text-lg font-semibold text-white text-center">
+                {movie.title}
+              </h2>
+              <button
+                onClick={() => removeMovie(movie.id)}
+                className="bg-red-200 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
