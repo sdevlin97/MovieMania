5fe7b4f493c6da701b22c3bdaf906c2b50a70a72
Kevin Horta <kevinhorta3@gmail.com>
2023-11-27 11:58:40 -0500

    Tags now can be removed and added
    Tag list was transfered to Reccomendations page so it can go inbetween Tags and selected tags easier

--- prototype/src/components/Recommendations.jsx
+++ prototype/src/components/Recommendations.jsx
@@ -1,7 +1,46 @@
+// Recommendations.jsx
+import React, { useState } from "react";
 import Tags from "./Tags";
 import SelectedTags from "./SelectedTags";
 import Card from "./Card";
 // SelectedTags is the same component has Tags, just to differ them here since they have diffrent hard coded data
+
+const AllTags = [
+  { name: "Superhero" },
+  { name: "Adventure" },
+  { name: "Action" },
+  { name: "Dark" },
+  { name: "DC" },
+  { name: "Marvel" },
+  { name: "Comic" },
+  { name: "Secret" },
+  { name: "Romance" },
+  { name: "Science Fiction" },
+  { name: "Fantasy" },
+  { name: "Horror" },
+  { name: "Comedy" },
+  { name: "Drama" },
+  { name: "Thriller" },
+  { name: "Mystery" },
+  { name: "Animation" },
+  { name: "Family" },
+  { name: "Historical" },
+  { name: "Western" },
+  { name: "Musical" },
+  { name: "War" },
+  { name: "Spy" },
+  { name: "Post-Apocalyptic" },
+  { name: "Time Travel" },
+  { name: "Aliens" },
+  { name: "Robots" },
+  { name: "Magical" },
+  { name: "Sports" },
+  { name: "Heist" },
+  { name: "Supernatural" },
+  { name: "Coming of Age" },
+  { name: "Disaster" },
+];
+
 function Recommendations() {
   const containerStyle = {
     backgroundImage: `url('./pawel-czerwinski-XM1YUUVXj64-unsplash.jpg')`,
@@ -11,19 +50,39 @@ function Recommendations() {
     minHeight: "100vh",
   };
 
+  const [availableTags, setAvailableTags] = useState(AllTags);
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const handleTagClick = (tagName, action) => {
+    if (action === "add") {
+      setSelectedTags([...selectedTags, tagName]);
+      setAvailableTags(availableTags.filter((tag) => tag.name !== tagName));
+    } else if (action === "remove") {
+      setSelectedTags(selectedTags.filter((tag) => tag !== tagName));
+      setAvailableTags([...availableTags, { name: tagName }]);
+    }
+  };
+
   return (
     <>
       <div style={containerStyle} className="relative bg-black">
         <div className="text-white flex items-center h-20 p-2 lg:px-4 bg-black ">
           Tags:
-          <Tags></Tags>
+          <Tags
+            availableTags={availableTags}
+            selectedTags={selectedTags}
+            onTagClick={handleTagClick}
+          />
         </div>
         <div className="text-white flex items-center h-20 p-2 lg:px-4 bg- bg-black">
           Selected Tags:
-          <SelectedTags></SelectedTags>
+          <SelectedTags
+            selectedTags={selectedTags}
+            onTagClick={handleTagClick}
+          />
         </div>
         <div className="backdrop-blur ">
-          <Card></Card>
+          <Card />
         </div>
         <div className="flex flex-col items-center justify-center pt-4 pb-20 backdrop-blur">
           <input
